1ef4549bfb3175a22d883e25e83357ab292d62fb
08april06 <hujab0406@gmail.com>
2026-04-06 14:36:34 +0900

    TodoListApp HTML

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,5 +1,26 @@
 function TodoListApp() {
-
+    return (
+        <div>
+            <h1>ToDo List</h1>
+            <form>
+                <input type="text"></input><button>Add</button>
+            </form>
+            <ul>
+                <li>
+                    <input type="checkbox" name="" id=""></input> 
+                    <label htmlFor="">놀기 </label>
+                    <button>✏️</button> 
+                    <button>❌</button>
+                </li>
+                <li>
+                    <input type="checkbox" name="" id=""></input> 
+                    <label htmlFor="">집가기 </label>
+                    <button>✏️</button> 
+                    <button>❌</button>
+                </li>
+            </ul>
+        </div>
+    )
 }
 
-export default TodoListApp
+export default TodoListApp;
